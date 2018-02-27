dc817306e61fca7d49cb8eb0067230323b580e04
Johanna Schreiner <jschreinerdesign@gmail.com>
2018-02-27 16:36:50 +1100

    pins draggable and resizable

--- app/components/Pin.js
+++ app/components/Pin.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Pin.scss'
+import Rnd from 'react-rnd'
 
 export default class Pin extends React.Component {
 
@@ -7,46 +7,15 @@ export default class Pin extends React.Component {
     super(props)
   }
 
-  componentDidMount() {
-
-    interact('.resize-drag')
-      .draggable({
-        onmove: window.dragMoveListener,
-        restrict: {
-          restriction: 'parent',
-          elementRect: { top: 0, left: 0, bottom: 0, right: 0 }
-        },
-      })
-      .resizable({
-        // resize from all edges and corners
-        edges: { left: true, right: true, bottom: true, top: true },
-
-        // keep the edges inside the parent
-        restrictEdges: {
-          outer: 'parent',
-          endOnly: true,
-        },
-
-
-        inertia: true,
-      })
-      .on('resizemove', function (event) {
-        var target = event.target,
-            x = (parseFloat(target.getAttribute('data-x')) || 0),
-            y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-        // update the element's style
-        target.style.width  = event.rect.width + 'px';
-        target.style.height = event.rect.height + 'px';
-      });
-
-  }
-
   render() {
-    return <div className ="resize-container">
-        <div className ="resize-drag">
-        </div>
-      </div>
+    return <Rnd
+      default={{
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100,
+      }}
+><div className = "plswrk"></div></Rnd>
   }
 
 }
